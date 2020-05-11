a47c0272b7cf9d21cd684031167c28b6c336ea98
Srijoy-Chaki <63344719+Srijoy-Chaki@users.noreply.github.com>
2020-05-11 16:41:19 +0530

    created recursion.c
    This displays the numbers as prime or not using recursion.

---
+++ recursion.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int primeno(int,int);
+void main()
+{
+int num,check;
+clrscr();
+printf("Enter a number to check whether it is prime or not: ");
+scanf("%d",&num);
+check=primeno(num,num/2);
+if(check==1)
+printf("%d is a prime number."num);
+else
+printf("%d is not a prime number",num);
+getch();
+}
+int primeno(int num,int i)
+{
+ if(i==1)
+   return 1;
+ else
+ {
+  if(num%i==0)
+  return 0;
+ else
+  return primeno(num,i-1);
+ }
+}
+
+
